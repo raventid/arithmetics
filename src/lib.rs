d94b803759b49d7c6767ce647efd99f20bd1b15c
raventid <juliankul@gmail.com>
2026-03-04 22:32:20 +0800

    remove profilers

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 pub mod precision;
 pub mod validation;
-pub mod profiling;
 pub mod error_analysis;
-pub mod profiler;
 pub mod regression;
 
 // Re-export commonly used types
 pub use validation::ValidationSuite;
-pub use profiling::PerformanceProfiler;
 pub use error_analysis::AdvancedErrorAnalyzer;
