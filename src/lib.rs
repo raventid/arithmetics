541844ed83dde895f2bdfed5ab90f2705f2c1010
raventid <juliankul@gmail.com>
2026-03-06 11:40:40 +0800

    remove error analysis module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 pub mod precision;
 pub mod validation;
-pub mod error_analysis;
 
 // Re-export commonly used types
 pub use validation::ValidationSuite;
-pub use error_analysis::AdvancedErrorAnalyzer;
